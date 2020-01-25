5710d801c3c4153d1f9ce1163fd6911c6844746e
Michael Vilain <michael@vilain.com>
2020-01-24 17:54:30 -0800

    use kibana 6.4

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,30 +8,30 @@
 #   include elk
 
 class elk {
-  class {'logstash':}
+  class {'logstash':
+    ensure      => present,
+    package_url => 'https://artifacts.elastic.co/downloads/logstash/logstash-6.2.3.deb',
+  }
   logstash::plugin {'logstash-input-beats':}
   logstash::configfile{'beats':
     source => 'puppet:///modules/elk/beats.conf',
   }
 
 
-#  class { 'elastic_stack::repo':
-#    version => 6,
-#  }
-
+# V7 elastic search doesn't work here, so install 6.2.4
   include ::java
   class { 'elasticsearch':
-    jvm_options => ['-Xms256m','-Xmx256m'],
-    status      => enabled,
     ensure      => present,
+    status      => enabled,
+    jvm_options => ['-Xms256m','-Xmx256m'],
     package_url => "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.deb",
-#    version     => '6.2.4',
   }
   elasticsearch::instance { 'es-01': }
 
 
   class {'kibana':
-    config => {
+    package_url => 'https://artifacts.elastic.co/downloads/kibana/kibana-6.4.0-amd64.deb',
+    config      => {
       'server.host' => '0.0.0.0',
     }
   }
