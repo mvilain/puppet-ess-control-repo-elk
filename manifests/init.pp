bbcc087685596f5d74d256b03a82d7ecc46b7f2d
Michael Vilain <michael@vilain.com>
2020-01-26 12:44:48 -0800

    change to version 6.8.6 stack

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@
 class elk {
   class {'logstash':
     ensure      => present,
-    package_url => 'https://artifacts.elastic.co/downloads/logstash/logstash-6.2.3.deb',
+    package_url => 'https://artifacts.elastic.co/downloads/logstash/logstash-6.8.6.deb',
   }
   logstash::plugin {'logstash-input-beats':}
   logstash::configfile{'beats':
@@ -18,20 +18,20 @@ class elk {
   }
 
 
-# V7 elastic search doesn't work here, so install 6.2.4
+# V7 elastic search doesn't work here, so install 6.8.6
   include ::java
   class { 'elasticsearch':
     ensure      => present,
     status      => enabled,
     jvm_options => ['-Xms256m','-Xmx256m'],
-    package_url => "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.4.deb",
+    package_url => "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.8.6.deb",
   }
   elasticsearch::instance { 'es-01': }
 
 
   class {'kibana':
 #    package_url => 'https://artifacts.elastic.co/downloads/kibana/kibana-6.4.0-amd64.deb',
-    ensure      => '6.4.0',
+    ensure      => '6.8.6',
     config      => {
       'server.host' => '0.0.0.0',
     }
