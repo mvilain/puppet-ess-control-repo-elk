d851fab82c569795c960a3487f6f3c846755aef4
Michael Vilain <michael@vilain.com>
2020-01-26 18:53:55 -0800

    add filebeat template

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -4,8 +4,10 @@
 #
 # @example
 #   include elk::filebeat
-class elk::filebeat {
-
+class elk::filebeat (
+  $logstash_server = '127.0.0.1',
+  $logstash_port   = '5044',
+) {
   include elastic_stack::repo
 
   package{'filebeat':
@@ -16,5 +18,14 @@ class elk::filebeat {
     ensure  => 'running',
     enable  => true,
     require => Package['filebeat'],
+  }
+
+  file{'/etc/filebeat/filebeat.yml':
+    ensure => file,
+    content => epp('elk/filebeat.yml.epp', {
+      logstash_server => $logstash_server,
+      logstash_port   => $logstash_port,
+      })
+    )
   }
 }
